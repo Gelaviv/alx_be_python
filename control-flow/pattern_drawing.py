aef7b7c9367208de779ae7280ce25d223323ed64
Gelaviv <odunsiamarachi@gmail.com>
2025-06-02 00:25:59 +0100

    Adding the updated code

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -14,19 +14,3 @@ while row < size:
 
     print()
     row += 1
-
-
-# # Initialize the row counter
-# row = 0
-
-# # Use a while loop to iterate through each row
-# while row < size:
-#     # Use a for loop to print asterisks in the current row
-#     for _ in range(size):
-#         print("*", end="")
-    
-#     # Move to the next line after completing the row
-#     print()
-    
-#     # Increment the row counter
-#     row += 1
