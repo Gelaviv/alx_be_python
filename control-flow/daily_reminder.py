df826cd20d66becec6ddee53039c9d18a7eaccd8
Gelaviv <odunsiamarachi@gmail.com>
2025-06-02 01:23:30 +0100

    Running a check to see why my code is failing

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,45 @@
+# task = input("Enter your task:").lower()
+# priority = input("Enter priority level (high/medium/low):").lower()
+# time_bound = input("Priority (high/medium/low):").lower()
+
+# match priority:
+#     case "high":
+#         if time_bound == "yes":
+#             print(f"{task} is a high priority task and requires immediate attention today!")
+#         elif time_bound == "no":
+#             print(f"{task} is a high priority task and needs to be completed within the week!")
+#     case "medium":
+#         if time_bound == "yes":
+#             print(f"{task} is a medium priority task that needs to be completed within the next two weeks!")
+#         elif time_bound == "no":
+#             print(f"{task} is a medium priority task that needs to be completed before next month!")
+#     case "low":
+#         if time_bound == "yes":
+#             print(f"{task} is a low priority task. Ensure it is completed before the year runs out!")
+#         elif time_bound == "no":
+#             print(f"{task} is a is a low priority task. Consider completing it when you have free time.") 
+#     case _:
+#         print("You have provided an invalid input")
+
 task = input("Enter your task:").lower()
-priority = input("Enter priority level (high/medium/low):").lower()
-time_bound = input("Priority (high/medium/low):").lower()
+priority = input("Priority (high/medium/low):").lower()
+time_bound = input("Is it time-bound? (yes/no):").lower()
 
 match priority:
     case "high":
-        if priority == "high" and time_bound == "yes":
+        if time_bound == "yes":
             print(f"{task} is a high priority task and requires immediate attention today!")
-        elif priority == "high" and time_bound == "no":
+        else:
             print(f"{task} is a high priority task and needs to be completed within the week!")
     case "medium":
-        if priority == "medium" and time_bound == "yes":
+        if time_bound == "yes":
             print(f"{task} is a medium priority task that needs to be completed within the next two weeks!")
-        elif priority == "medium" and time_bound == "no":
+        else:
             print(f"{task} is a medium priority task that needs to be completed before next month!")
     case "low":
-        if priority == "low" and time_bound == "yes":
+        if time_bound == "yes":
             print(f"{task} is a low priority task. Ensure it is completed before the year runs out!")
-        elif priority == "low" and time_bound == "no":
-            print(f"{task} is a is a low priority task. Consider completing it when you have free time.") 
+        else:
+            print(f"{task} is a low priority task. Consider completing it when you have free time.") 
     case _:
         print("You have provided an invalid input")
