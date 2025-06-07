30351efd19993af4a782ec6148d08c72d846fb61
Gelaviv <odunsiamarachi@gmail.com>
2025-06-07 15:50:33 +0100

    Fixed operator issue

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,16 @@
 def perform_operation(num1, num2, operation):
     match operation:
         case "add":
-            addition = num1 + num2
-            return addition
+            return num1 + num2
         case "subtract":
-            subtraction = num1 - num2
-            return subtraction
+            return num1 - num2
 
         case "multiply":
-            multiplication = num1 * num2
-            return multiplication
+            return num1 * num2
         case "divide":
-            division = num1 / num2
-            if num1 % num2 != 0:
-                print("Numbers are not divisible by zero")
-            else:
-                return division
+            if num2 == 0:
+                return "Cannot divide by Zero"
+        
+            return num1 / num2
         case _:
-            print("You have entered an invalid operation")
+            return "You have entered an invalid operation"
