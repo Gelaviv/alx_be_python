8feaadd88d2414d4a18e3f43eced015fe7db7e5f
Gelaviv <odunsiamarachi@gmail.com>
2025-06-21 17:39:24 +0100

    Exploring Polymorphism and Method Overriding task

--- oop/main.py
+++ oop/main.py
@@ -1,5 +1,7 @@
 from book_class import Book
 from library_system import Book, EBook, PrintBook, Library
+from polymorphism_demo import Shape, Rectangle, Circle
+
 
 # Magic methods test code
 def main():
@@ -35,6 +37,24 @@ def main():
 
     # List all books in the library
     my_library.list_books()
+
+
+
+
+    # Polymorphism test code
+
+def main():
+    shapes = [
+        Rectangle(10, 5), 
+        Circle(7)
+    ]
+
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+
+
+
 
 if __name__ == "__main__":
     main()
